f0f7e0fcea727dbd195931334b5c5ca5ec709234
Muhammad Muneeb Khan <160416689+MuhammadMuneeb0@users.noreply.github.com>
2024-03-23 04:36:26 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,111 @@
+// 1. Declare and initialize an empty multidimensional array.
+// (Array of arrays)
+
+                        //    Code 1:
+// var multidimensionalArr=[];
+
+// multidimensionalArr.push=([]);
+// multidimensionalArr.push=([]);
+
+
+// multidimensionalArr[0]=1;
+// multidimensionalArr[0]=2
+// multidimensionalArr[1]=3;
+// multidimensionalArr[1]=4;
+
+// console.log(multidimensionalArr);
+
+
+// 2. Declare and initialize a multidimensional array
+// representing the following matrix:
+
+// [0, 1, 2, 3]
+// [1, 0, 1, 2]
+// [2, 1, 0, 1]
+  
+                                // Code 2:
+
+// var matrix=[
+//     [0,1,2,3],[1,0,1,2],[2,1,0,1]
+
+// ]
+
+// console.log(matrix[0]);
+// console.log(matrix[1]);
+// console.log(matrix[2]);
+
+
+
+// Write a program to print numeric counting from 1 to 10.
+
+                                  // Code 3:
+
+// for (i=1;i<=10;i++){
+//     console.log(i)
+
+// }
+
+
+
+            // Write a program to print mutiplication table of any number using for loop.Table number and length should be taken as input from user.
+
+                         // code 4:
+
+// var num=+prompt("Enter the number");
+// for(var i=1;i<=10;i++){
+//  document.write(num+ "*" +i+ " = "+i*num+"<br>")
+// }
+
+
+
+// 5:Write a program to print item of the followingarray using for loop:
+// fruits=["apple ","banana ","orange","mango","strawberry"] 
+
+
+// var fruits=["apple","banana","mango","orange","strawberry"];
+// console.log(fruits);
+
+// 6:Generate the following series in your browser:
+// a:counting 1 to 15
+
+
+                    //    code6:
+
+
+// for(i=1;i<=15;i++){
+//     console.log(i);
+// }
+
+
+// b:reverse counting 10 to 1:
+
+
+                              // code:
+
+
+
+// for(i=10;i>=1;i--){
+//     console.log(i)
+// }
+
+// Even numbers from 0 to 20
+
+                                // Code
+                               
+                                // Code 
+                                
+                                
+                                // for (var i=0;i<=20;i+=2){
+                                //     console.log(i)
+                                // }                             
+                                
+                                
+                                
+                                // d. Generate the following: Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19
+                                
+                                                            //    Code:
+                                
+                                
+                                // for(i=1;i<=19;i+=2){
+                                //     console.log(i)
+                                // }
